7bc9ce9d245a9cf7cd24b6eb0dc240303294447a
Richard Mortier <mort@cantab.net>
2011-11-14 16:55:36 +0000

    [mkpdf] don't rebuild if pdf exists

--- mkpdf.sh
+++ mkpdf.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env bash
 set -ex
 
-grep -v '^```' README.md | markdown2pdf -o README.pdf
+[ ! -r README.pdf ] &&
+  grep -v '^```' README.md | markdown2pdf -o README.pdf
+
+[ ! -r 05-guestbook.pdf ] &&
+  grep -v '^```' 05-guestbook.md | markdown2pdf -o 05-guestbook.pdf
 
 for d in * ; do 
-  [ -d "$d" -a -f "$d/README.md" ] && (
+  [ -d "$d" -a -f "$d/README.md" -a ! -r "$d.pdf" ] && (
     cd $d && grep -v '^```' README.md | markdown2pdf -o ../$d.pdf
   )
 done
